accfdcd30473ba1cdffac6c4d4ab6487d81dc03a
Pamavoc <58072737+Pamavoc@users.noreply.github.com>
2020-06-04 17:22:56 +0200

    checkbox filter

--- src/index.js
+++ src/index.js
@@ -4,32 +4,63 @@ console.log("hello world!");
 import { annotate } from 'rough-notation';
 
 
-
-
-
-
-
 //isotope
 var $ = require('jquery');
 var jQueryBridget = require('jquery-bridget');
 var Isotope = require('isotope-layout');
-
-
 jQueryBridget( 'isotope', Isotope, $ );
+
+
+
+
+
 var $grid = $('.grid');
 
 $('.grid').isotope({
-  // options
+  
   itemSelector: '.grid-item',
   layoutMode: 'vertical',
  
 });
 
 
+var $checkboxes = $('.filter-all input');
+
+$checkboxes.change( function() {
+  
+  var inclusives = [];
+  
+  $checkboxes.each( function( i, elem ) {
+    
+    if ( elem.checked ) {
+      inclusives.push( elem.value );
+    }
+  });
+
+  var filterValue = inclusives.length ? inclusives.join(', ') : '*';
+
+  $grid.isotope({ filter: filterValue })
+});
+
+
+
+
+
+/*checkbox filter
+$('.filter-all').on('click', 'input', function() {
+  var filterValue = this.value;
+  $grid.isotope({ filter: filterValue });
+});
+*/
+
+
+/*button filter
 $('.filter-all').on('click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
+*/
+
 
 
 
